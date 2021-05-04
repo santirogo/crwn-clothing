72b6fc240e4817176dff668e991a44a50e097ba9
Andres Santiago Romero <andres.romero1@ibm.com>
2021-05-04 15:46:12 -0500

    Se implementa autenticación con Firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,32 @@ const config = {
     messagingSenderId: "246009964304",
     appId: "1:246009964304:web:a9529b2a7396cefef4e795",
     measurementId: "G-91695KGSSL"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.error('error creating user', error.message)
+        }
+    }
+
+    return userRef;
 }
 
 firebase.initializeApp(config);
